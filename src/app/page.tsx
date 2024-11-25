92ac08a8827087ca86f1444a8d7039bf3b5b444b
chani-s <slaterchana@gmail.com>
2024-11-25 13:03:49 +0200

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <img src="../../imgs/logo.png" alt="logo"/>
       <div className={styles.spinner}></div>
       <div className={styles.text}>coming soon</div>
     </div>
   );
 }
+
+
