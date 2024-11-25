27d015b0559d314738a2a1b105d8791fbc05c62f
chani-s <slaterchana@gmail.com>
2024-11-25 19:32:21 +0200

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+"use client";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <img src="../../imgs/logo.png" alt="logo"/>
-      <div className={styles.spinner}></div>
-      <div className={styles.text}>coming soon</div>
+    <div className={styles.container}>
+      <img src="/imgs/logo0.png" alt="logo" className={styles.logo} />
+
+      <div className={styles.page}>
+        <div className={styles.spinner}>
+          <div className={styles.text}>coming soon</div>
+        </div>
+      </div>
     </div>
   );
 }
