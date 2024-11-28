39f5bd662f40ef3cbaf5b21c1b8d3d297efd80f6
chani-s <slaterchana@gmail.com>
2024-11-28 13:01:09 +0200

    start components

---
+++ src/app/components/chatPages/MainChat/MainChat.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./MainChat.module.css"
+
+
+const MainChat = () =>{
+    return (
+        <div className={styles.mainChat}>
+            Main Chat
+        </div>
+    )
+}
+
+export default MainChat;
