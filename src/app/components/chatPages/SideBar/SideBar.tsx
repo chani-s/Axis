afbb20b183b7a733acbe073e3f384b5cddf00c7d
AyalaSprei <ayala0bsd@gmail.com>
2024-11-28 14:26:52 +0200

    continue designing

--- src/app/components/chatPages/SideBar/SideBar.tsx
+++ src/app/components/chatPages/SideBar/SideBar.tsx
@@ -1,16 +1,35 @@
-import React from "react";
-import styles from "./SideBar.module.css"
+import React, { useState } from "react";
+import { Conversation } from "@/app/models/Conversation";
+import styles from "./SideBar.module.css";
 
-const SideBar = () =>{
-    return (
-        <div className={styles.sideBar}>
-            <div className={styles.inputs}>
-                <input className={styles.input} type="text" placeholder="חיפוש חברה חדשה..."/>
-                <input className={styles.input} type="text" placeholder=" חפש בצאטים..." />
-                הצאטים שלך
-            </div>
+const SideBar = () => {
+
+    const [conversations, setConversations] = useState<Conversation[]>([]);  // Initialize as an empty array
+const arr=[1,2,3];
+
+return (
+    <div className={styles.sideBar}>
+      <div className={styles.inputs}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="חיפוש חברה חדשה..."
+        />
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="חפש בצאטים..."
+        />
+      </div>
+      <p className={styles.yourChatsP}>הצאטים שלך</p>
+      {arr.map((c) => (
+        <div className={styles.conversationItem}>
+          <img className={styles.profileCircle}/>
+          <p className={styles.name}>comapny</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default SideBar;
