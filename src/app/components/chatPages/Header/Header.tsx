fc0350dbe43e3b9c93d6263ccf81e755b47d4a4f
chani-s <slaterchana@gmail.com>
2024-11-28 13:01:09 +0200

    start components

---
+++ src/app/components/chatPages/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+return(
+    <div className={styles.header}>
+        <h1>Profile</h1>
+    </div>
+)
+
+}    
+export default Header;
