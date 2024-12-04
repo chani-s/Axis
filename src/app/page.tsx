c20b1d024b2ae8f752fbf2d53ddb8b8f6878bcca
chani-s <slaterchana@gmail.com>
2024-12-04 10:30:00 +0200

    links style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className={styles.container}>
-        <Link className={styles.navLink} href="/login">LogIn</Link>
-        <Link className={styles.navLink} href="/signup">SignUp</Link>
-        <Link className={styles.navLink} href="/chat/user">Main Chat</Link>
-
-      {/* <img src="/imgs/logo0.png" alt="logo" className={styles.logo} />
-      <div className={styles.page}>
+      <img src="/imgs/logo0.png" alt="logo" className={styles.logo} />
+      <Link className={styles.navLink} href="/login">כניסה</Link>
+      <Link className={styles.navLink} href="/signup">הרשמה</Link>
+      <Link className={styles.navLink} href="/chat/user">עמוד צ'אט</Link>
+      {/* <div className={styles.page}>
       <div className={styles.text}>coming soon</div>
 
           <img className={styles.spinner} src="/imgs/working.png" alt="work"></img>
       </div> */}
+
     </div>
   );
 }
