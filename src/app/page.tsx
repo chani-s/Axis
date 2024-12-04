ff00fb15fff8f158d45b58d41b1cfd6d2799e33a
chani-s <slaterchana@gmail.com>
2024-12-04 09:25:09 +0200

    add basic links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 import styles from "./page.module.css";
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src="/imgs/logo0.png" alt="logo" className={styles.logo} />
+        <Link className={styles.navLink} href="/login">LogIn</Link>
+        <Link className={styles.navLink} href="/signup">SignUp</Link>
+        <Link className={styles.navLink} href="/chat/user">Main Chat</Link>
+
+      {/* <img src="/imgs/logo0.png" alt="logo" className={styles.logo} />
       <div className={styles.page}>
       <div className={styles.text}>coming soon</div>
 
           <img className={styles.spinner} src="/imgs/working.png" alt="work"></img>
-      </div>
+      </div> */}
     </div>
   );
 }
