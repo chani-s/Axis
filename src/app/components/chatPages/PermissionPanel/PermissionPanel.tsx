1ce5de10d47268ac06f641cd0f2afaf98a95565b
chani-s <slaterchana@gmail.com>
2024-12-04 12:05:16 +0200

    permission try

--- src/app/components/chatPages/PermissionPanel/PermissionPanel.tsx
+++ src/app/components/chatPages/PermissionPanel/PermissionPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaUser, FaPhone, FaHome, FaIdCard, FaCreditCard } from "react-icons/fa";
 import styles from "./PermissionPanel.module.css";
 
@@ -9,13 +9,34 @@ const iconsMapping = {
     bank: <FaCreditCard />,
 };
 
+const defaultPermissions = {
+    phone: false,
+    address: false,
+    id: false,
+    bank: false,
+};
+
 const PermissionPanel = () => {
-    const [permissions, setPermissions] = useState({
-        phone: false,
-        address: false,
-        id: false,
-        bank: false,
-    });
+
+    const [permissions, setPermissions] = useState<typeof defaultPermissions>(defaultPermissions);
+
+    // const [permissions, setPermissions] = useState({
+    //     phone: false,
+    //     address: false,
+    //     id: false,
+    //     bank: false,
+    // });
+
+    useEffect(() => {
+        const storedPermissions = localStorage.getItem("permissions");
+        if (storedPermissions) {
+            setPermissions(JSON.parse(storedPermissions));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("permissions", JSON.stringify(permissions));
+    }, [permissions]);
 
     const togglePermission = (key: keyof typeof permissions) => {
         setPermissions((prev) => ({
@@ -25,10 +46,10 @@ const PermissionPanel = () => {
     };
 
     const descriptions: Record<keyof typeof permissions, string> = {
-        phone: "הרשאה לגשת למספר הטלפון",
-        address: "הרשאה לגשת לכתובת",
-        id: "הרשאה לגשת לתעודת הזהות",
-        bank: "הרשאה לגשת לפרטי חשבון הבנק"
+        phone: "הרשאת גישה למספר הטלפון",
+        address: "הרשאת גישה לכתובת",
+        id: "הרשאת גישה לתעודת הזהות",
+        bank: "הרשאת גישה לפרטי חשבון הבנק"
     };
 
     return (
