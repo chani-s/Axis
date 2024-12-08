9decea0f2aab4a7f622f5d68cf533c7abf54239c
chani-s <slaterchana@gmail.com>
2024-12-08 12:14:19 +0200

    send details to header

--- src/app/components/Entrance/UserEntrance.tsx
+++ src/app/components/Entrance/UserEntrance.tsx
@@ -22,22 +22,8 @@ export const Entrance = ({ type }: any) => {
         mutationFn: signUpUser,
         onSuccess: (data: any) => {
             console.log(data);
-            if (data.userId != "") {
-                const userDetails = {
-                    _id: data.userDetails._id,
-                    email: data.userDetails.email,
-                    google_auth: data.userDetails.google_auth || false,
-                    user_type: data.userDetails.user_type,
-                };
-                setUserDetails(data.userDetails);
-                console.log(userDetails);
-                if (data.userDetails.user_type == "user")
-                    router.push('/chat/user');
-                if (data.userDetails.user_type == "representative")
-                    router.push('/chat/representative');
-                if (data.userDetails.user_type == "management")
-                    router.push('/chat/management');
-            }
+            if (data.userId != "") 
+                setDetails(data);
             else {
                 alert("שגיאה בהרשמה נסה שוב או התחבר");
             }
@@ -51,23 +37,9 @@ export const Entrance = ({ type }: any) => {
         mutationFn: loginUser,
         onSuccess: (data) => {
             console.log(data);
-            if (data.userId != "") {
-                const userDetails = {
-                    _id: data.userDetails._id,
-                    email: data.userDetails.email,
-                    google_auth: data.userDetails.google_auth || false,
-                    user_type: data.userDetails.user_type,
-                };
-                setUserDetails(userDetails);
-                if (data.userDetails.user_type == "user")
-                    router.push('/chat/user');
-                if (data.userDetails.user_type == "representative")
-                    router.push('/chat/representative');
-                if (data.userDetails.user_type == "management")
-                    router.push('/chat/management');
-
-                console.log(userDetails);
-            } else {
+            if (data.userId != "") 
+                setDetails(data);
+            else {
                 alert("אימייל או סיסמא שגויים");
             }
         },
@@ -76,6 +48,42 @@ export const Entrance = ({ type }: any) => {
 
         },
     });
+
+    const setDetails = (data: any) => {
+        console.log("Data passed to setDetails:", data);
+
+        if (data.userDetails.user_type == "user") {
+            const userDetails = { // Details should be update according to types and popup new details
+                _id: data.userDetails._id,
+                email: data.userDetails.email,
+                google_auth: data.userDetails.google_auth || false,
+                user_type: data.userDetails.user_type,
+            };
+            setUserDetails(userDetails);
+            router.push('/chat/user');
+        }
+        if (data.userDetails.user_type == "representative") {
+            const userDetails = {
+                _id: data.userDetails._id,
+                email: data.userDetails.email,
+                google_auth: data.userDetails.google_auth || false,
+                user_type: data.userDetails.user_type,
+            };        
+            setUserDetails(userDetails);
+            router.push('/chat/representative');
+        }
+        if (data.userDetails.user_type == "manager") {
+            const userDetails = {
+                _id: data.userDetails._id,
+                email: data.userDetails.email,
+                google_auth: data.userDetails.google_auth || false,
+                user_type: data.userDetails.user_type,
+            };
+            setUserDetails(userDetails);
+            router.push('/chat/manager');
+        }
+        console.log(userDetails);
+    }
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
