ce626c4728220fb4482bb3ba0ef34353fcacd2e0
AyalaSprei <ayala0bsd@gmail.com>
2024-12-08 11:20:44 +0200

    add secrets

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,12 @@ const nextConfig = {
   env: {
     PUBLIC_DB_CONNECTION:
       "mongodb+srv://slaterchana:EbFAGAfbZqNMEzWZ@axis.lhftj.mongodb.net/?retryWrites=true&w=majority&appName=Axis",
-      PUSHER_APP_ID:"1907952",
-      PUSHER_KEY:"56a1d165c6599339ee60",
-secret:"7b94c0cb7cc94d40b68a",
-cluster:"eu"
+    PUSHER_APP_ID: "1907952",
+    PUSHER_KEY: "56a1d165c6599339ee60",
+    PUSHER_SECRET: "7b94c0cb7cc94d40b68a",
+    PUSHER_CLUSTER: "eu",
+    NEXT_PUBLIC_PUSHER_KEY: "56a1d165c6599339ee60",
+    NEXT_PUBLIC_PUSHER_CLUSTER: "eu",
   },
 };
 export default nextConfig;
