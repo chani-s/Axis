59e5875a97e36cc0d716717669fb4059c65b2495
chani-s <slaterchana@gmail.com>
2024-12-09 10:25:27 +0200

    add open page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+import { Fredoka } from '@next/font/google';
+
+const roboto = Fredoka({
+  weight: ['400', '400'],
+  subsets: ['latin'],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 const queryClient = new QueryClient();
 
 export default function RootLayout({
@@ -25,11 +22,22 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="favicon.ico" />
-        <title>Axis</title> 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700" rel="stylesheet"></link>
+        <title>Axis</title>
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={roboto.className}>
         <QueryClientProvider client={queryClient}>
           {children}
+          {/* <div className={styles.container}> */}
+
+          {/* <img src="/imgs/logo0.png" alt="logo" className={styles.logo} />
+      <Link className={styles.navLink} href="/about">אודות</Link>
+      <Link className={styles.navLink} href="/login">כניסה</Link>
+      <Link className={styles.navLink} href="/signup">הרשמה</Link>
+      <Link className={styles.navLink} href="/chat/user">עמוד צ'אט</Link>
+      */}
         </QueryClientProvider>
       </body>
     </html>
