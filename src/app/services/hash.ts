8a7f1a2013bc6fda35ed990f96456d151a19c23e
rut-te <rutite261@gmail.com>
2024-12-09 14:08:46 +0200

    add register with google

--- src/app/services/hash.ts
+++ src/app/services/hash.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 
-
 export async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10; 
   const hashedPassword = await bcrypt.hash(password, saltRounds);
