c4a873ac12de1055c7d2199c5ee5fc1e900eb4be
rut-te <rutite261@gmail.com>
2024-12-16 13:15:11 +0200

    add forgot password and option of send email

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,10 @@ const nextConfig = {
     PUSHER_APP_ID: "1908915",
     PUSHER_KEY: "ff054817599b88393e16",
     PUSHER_SECRET: "266445049ee3b1fb3ec4",
-    PUSHER_CLUSTER: "ap2"
+    PUSHER_CLUSTER: "ap2",
+    EMAIL_ADDRESS: "axiss1chat@gmail.com",
+    EMAIL_PASSWORD: "ipun gfeb xesp wrko",
+    SECRET_KEY : "m10r07w24",
 
   },
 };
