b2d5226f7ddc51d819dfb81a2e66384d53ca74cf
chani-s <slaterchana@gmail.com>
2025-01-05 11:11:27 +0200

    upgrade vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,18 @@ const nextConfig = {
   env: {
     PUBLIC_DB_CONNECTION:
       "mongodb+srv://slaterchana:EbFAGAfbZqNMEzWZ@axis.lhftj.mongodb.net/?retryWrites=true&w=majority&appName=Axis",
-    PUSHER_APP_ID: "1908915",
-    PUSHER_KEY: "ff054817599b88393e16",
-    PUSHER_SECRET: "266445049ee3b1fb3ec4",
+    PUSHER_APP_ID: "1920966",
+    PUSHER_KEY: "78f3e87c9779c64d7746",
+    PUSHER_SECRET: "68dc2418e1b32de7dde0",
     PUSHER_CLUSTER: "ap2",
     EMAIL_ADDRESS: "axiss1chat@gmail.com",
     EMAIL_PASSWORD: "ipun gfeb xesp wrko",
-    SECRET_KEY : "m10r07w24",
-    TOKEN_SECRET_KEY:"m10r07w24",
+    SECRET_KEY: "m10r07w24",
+    TOKEN_SECRET_KEY: "m10r07w24",
     CLOUDINARY_CLOUD_NAME: "dmyly6bfr",
     CLOUDINARY_API_KEY: "966972565264862",
     CLOUDINARY_API_SECRET: "YEpeBDvOx_7V4AcRFqbNzWXMDoM",
-    MANAGER_EMAIL:"axiss1chat@gmail.com"
+    MANAGER_EMAIL: "axiss1chat@gmail.com"
 
   },
 };
